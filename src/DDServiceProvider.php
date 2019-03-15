702511426349df43c1a566b048f3114e3068eacd
Firose Hussain <github@firumon.com>
2019-03-15 10:47:26 +0530

    init

---
+++ src/DDServiceProvider.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Milestone\DD;
+
+use Illuminate\Support\ServiceProvider;
+
+class DDServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+}
