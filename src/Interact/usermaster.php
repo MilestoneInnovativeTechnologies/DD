22b8e15ffacc8a50c04bbb6a669ed87fc31c4602
Firose Hussain <github@firumon.com>
2019-04-02 01:00:25 +0530

    Roles will be created if not exists

--- src/Interact/usermaster.php
+++ src/Interact/usermaster.php
@@ -4,10 +4,14 @@
 
     use Illuminate\Support\Str;
     use Milestone\Appframe\Model\Group;
+    use Milestone\Appframe\Model\Role;
     use Milestone\Interact\Table;
 
     class usermaster implements Table
     {
+        public $role_cache = null;
+        public $new_role_prefix = 'eplus_';
+
         public function getModel()
         {
             return Group::class;
@@ -43,5 +47,14 @@
 
         public function isRecordValid($record){
             return $record['ISGROUP'] === 'Y';
+        }
+        public function preActions(){
+            $this->role_cache = Role::pluck('name','id')->toArray();
+        }
+        public function isDone($record,$id){
+            $role_name = $this->new_role_prefix . $this->getName($record);
+            if(!in_array($role_name,$this->role_cache)) { $role = new Role; $role->unguard(); $role->create(['name' => $role_name, 'title' => $record['NAME']]); $this->preActions(); }
+            $role = Role::find(array_search($role_name,$this->role_cache));
+            $role->Groups()->attach($id);
         }
     }
