9d8f825f3fdada8d8259ce31c70bcd0cad0fc67f
Firose Hussain <github@firumon.com>
2019-04-03 18:03:49 +0530

    Global scope added for ePlus users

--- src/Model/User.php
+++ src/Model/User.php
@@ -2,11 +2,18 @@
 
 namespace Milestone\SS\Model;
 
+use Illuminate\Database\Eloquent\Builder;
 use Milestone\Appframe\Model\User as AppframeUser;
 
 class User extends AppframeUser
 {
-    public function Area(){ $this->belongsToMany(Area::class,'area_user','user','area'); }
-    public function Settings(){ $this->hasMany(UserSetting::class,'user'); }
-    public function StoreAndArea(){ $this->hasMany(UserStoreArea::class,'user')->with(['Store','Area']); }
+    protected static function boot(){
+        parent::boot();
+        static::addGlobalScope('ExceptAppframeUsers',function(Builder $builder){
+            $builder->whereNotNull('reference');
+        });
+    }
+    public function Area(){ return $this->belongsToMany(Area::class,'area_user','user','area'); }
+    public function Settings(){ return $this->hasMany(UserSetting::class,'user'); }
+    public function StoreAndArea(){ return $this->hasMany(UserStoreArea::class,'user')->with(['Store','Area']); }
 }
