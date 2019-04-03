f86976231365569b21989881668f67f5acd57e3b
Firose Hussain <github@firumon.com>
2019-04-04 01:23:00 +0530

    Relation modified

--- src/Model/User.php
+++ src/Model/User.php
@@ -14,6 +14,6 @@ class User extends AppframeUser
         });
     }
     public function Area(){ return $this->belongsToMany(Area::class,'area_user','user','area'); }
-    public function Settings(){ return $this->hasMany(UserSetting::class,'user'); }
+    public function Settings(){ return $this->hasMany(UserSetting::class,'user')->with(['Settings']); }
     public function StoreAndArea(){ return $this->hasMany(UserStoreArea::class,'user')->with(['Store','Area']); }
 }
