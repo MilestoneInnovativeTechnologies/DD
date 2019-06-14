2b3cbad07a5e050a7ff76bd87dda869f794f8d77
Firose Hussain <github@firumon.com>
2019-06-14 09:47:08 +0530

    New scope added

--- src/Seeder/ResourceScopeTableSeeder.php
+++ src/Seeder/ResourceScopeTableSeeder.php
@@ -20,6 +20,7 @@ class ResourceScopeTableSeeder extends Seeder
             ->create([	'id' => '307102', 	'resource' => '305116', 	'name' => 'Assigned', 	'description' => 'The store and area whih is assigned to the requesting user', 	'method' => 'assigned', 											])
             ->create([	'id' => '307103', 	'resource' => '305114', 	'name' => 'Assigned', 	'description' => 'The records where area is assigned to the requester', 	'method' => 'assigned', 											])
             ->create([	'id' => '307104', 	'resource' => '305126', 	'name' => 'AssignedAreaCustomer', 	'description' => 'Sales orders of customers belongs to a area which assigned to a executive', 	'method' => 'assignedAreaCustomer', 											])
+            ->create([	'id' => '307105', 	'resource' => '305121', 	'name' => 'AssignedCustomerTransactions', 	'description' => 'Transactions related to customers who are assigned to the requesting executive', 	'method' => 'assignedCustomerTransactions', 											])
         ;
         \DB::statement('set foreign_key_checks = ' . $_);
     }
