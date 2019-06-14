e6708995fc441505bee0120eb3820b35717fb0ac
Firose Hussain <github@firumon.com>
2019-06-14 08:37:39 +0530

    Optimized

--- src/Interact/stock_transfer.php
+++ src/Interact/stock_transfer.php
@@ -3,12 +3,13 @@
     namespace Milestone\SS\Interact;
 
     use Milestone\Interact\Table;
+    use Milestone\SS\Model\StockTransfer;
 
     class stock_transfer implements Table
     {
         public function getModel()
         {
-            return \Milestone\SS\Model\StockTransfer::class;
+            return StockTransfer::class;
         }
 
         public function getImportAttributes()
