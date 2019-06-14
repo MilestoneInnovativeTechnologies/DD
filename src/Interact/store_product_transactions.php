4d30c6be1044baa2074d98f95e429b6dabea67d1
Firose Hussain <github@firumon.com>
2019-06-14 08:32:23 +0530

    User dependent corrected

--- src/Interact/store_product_transactions.php
+++ src/Interact/store_product_transactions.php
@@ -2,13 +2,16 @@
 
     namespace Milestone\SS\Interact;
 
+    use Illuminate\Support\Facades\Auth;
     use Milestone\Interact\Table;
+    use Milestone\SS\Model\StoreProductTransaction;
+    use Milestone\SS\Model\UserStoreArea;
 
     class store_product_transactions implements Table
     {
         public function getModel()
         {
-            return \Milestone\SS\Model\StoreProductTransaction::class;
+            return StoreProductTransaction::class;
         }
 
         public function getImportAttributes()
@@ -27,15 +30,24 @@
 
         public function getExportMappings()
         {
-            // TODO: Implement getExportMappings() method.
+            return ['id' => 'getProperReference'];
         }
 
         public function getExportAttributes()
         {
-            return ['store','product','direction','quantity','user','nature','date','type','_ref'];
+            return ['id','store','product','direction','quantity','user','nature','date','type','_ref'];
         }
 
         public function getProperReference($record){
             return $record['_ref'] ?: implode('',['U',$record['user'],'T',intval(microtime(true)*10000)]);
+        }
+
+        public function preExportGet($query){
+            if (request()->_user) Auth::loginUsingId(request()->_user); else return $query;
+            return $query->whereIn('store',UserStoreArea::assigned()->pluck('store')->all());
+        }
+        public function preExportUpdate($query){
+            if (request()->_user) Auth::loginUsingId(request()->_user); else return $query;
+            return $query->whereIn('store',UserStoreArea::assigned()->pluck('store')->all());
         }
     }
