d050669140b3cdafb9bdb9106927eac0aca3157b
Firose Hussain <github@firumon.com>
2019-06-14 07:35:56 +0530

    Store product transaction, purchase type added

--- src/Seeder/ProductTransactionTypeTableSeeder.php
+++ src/Seeder/ProductTransactionTypeTableSeeder.php
@@ -16,11 +16,12 @@ class ProductTransactionTypeTableSeeder extends Seeder
         $_ = \DB::statement('SELECT @@GLOBAL.foreign_key_checks');
         \DB::statement('set foreign_key_checks = 0');
         \Milestone\SS\Model\ProductTransactionType::truncate()
-            ->create([	'id' => '1', 	'name' => 'Load', 														])
-            ->create([	'id' => '2', 	'name' => 'Unload', 														])
-            ->create([	'id' => '3', 	'name' => 'Sale', 														])
-            ->create([	'id' => '4', 	'name' => 'Return', 														])
-            ->create([	'id' => '5', 	'name' => 'Other', 														])
+            ->create([	'id' => '1', 	'name' => 'Purchase', 														])
+            ->create([	'id' => '2', 	'name' => 'Sale', 														])
+            ->create([	'id' => '3', 	'name' => 'Return', 														])
+            ->create([	'id' => '4', 	'name' => 'Load', 														])
+            ->create([	'id' => '5', 	'name' => 'Unload', 														])
+            ->create([	'id' => '6', 	'name' => 'Other', 														])
         ;
         \DB::statement('set foreign_key_checks = ' . $_);
     }
