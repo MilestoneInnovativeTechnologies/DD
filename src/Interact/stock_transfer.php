c2302e21bf9dc2b794121eb83a84840055983919
Firose Hussain <github@firumon.com>
2019-06-14 10:13:48 +0530

    Corrected interact interface

--- src/Interact/stock_transfer.php
+++ src/Interact/stock_transfer.php
@@ -2,8 +2,10 @@
 
     namespace Milestone\SS\Interact;
 
+    use Illuminate\Support\Arr;
     use Milestone\Interact\Table;
     use Milestone\SS\Model\StockTransfer;
+    use Milestone\SS\Model\Transaction;
 
     class stock_transfer implements Table
     {
@@ -14,25 +16,42 @@
 
         public function getImportAttributes()
         {
-            return [];
+            return ['id','out','in','verified_by','verified_at'];
         }
 
         public function getImportMappings()
         {
-            return [];
+            return [
+                'out' => 'getTransactionOutID',
+                'in' => 'getTransactionInID'
+            ];
         }
 
         public function getPrimaryIdFromImportRecord($data)
         {
+            Arr::get(StockTransfer::where(['out' => $this->getTransactionOutID($data),'in' => $this->getTransactionInID($data)])->first(),'id');
         }
 
         public function getExportMappings()
         {
-            return [];
+            return [
+                'out' => 'getTransactionOutReference',
+                'in' => 'getTransactionInReference'
+            ];
         }
 
         public function getExportAttributes()
         {
             return ['id','out','in','verified_by','verified_at'];
+        }
+
+        public function getTransactionOutReference($record){ return Arr::get(Transaction::find($record['out']),'_ref'); }
+        public function getTransactionInReference($record){ return Arr::get(Transaction::find($record['in']),'_ref'); }
+
+        public function getTransactionOutID($record){ return Arr::get(Transaction::where('_ref',$record['out'])->first(),'id'); }
+        public function getTransactionInID($record){ return Arr::get(Transaction::where('_ref',$record['in'])->first(),'id'); }
+
+        public function preExportGet($query){
+            return $query->whereNull('in');
         }
     }
