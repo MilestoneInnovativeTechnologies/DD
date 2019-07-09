ac69e5f9fc67b882855f1f5ac629296e3e303fb1
Firose Hussain <github@firumon.com>
2019-07-09 18:48:07 +0530

    price -> amount in relation of transaction with products

--- src/Model/Transaction.php
+++ src/Model/Transaction.php
@@ -18,7 +18,7 @@ class Transaction extends Model
     protected $table = 'transactions';
 
     public function Details(){ return $this->hasMany(TransactionDetail::class,'transaction','id')->with(['Product']); }
-    public function Products(){ return $this->belongsToMany(StoreProductTransaction::class,'transaction_details','transaction','spt')->withPivot(['price','rate','tax','discount'])->with(['Product'])->withTimestamps(); }
+    public function Products(){ return $this->belongsToMany(StoreProductTransaction::class,'transaction_details','transaction','spt')->withPivot(['amount','rate','tax','discount'])->with(['Product'])->withTimestamps(); }
 
     public function scopeAssignedCustomerTransactions($Q){
         return $Q->whereIn('customer',User::find(request()->user()->id)->AreaCustomers->pluck('user')->all());
