2f397b4066583f67dcc1a8ff78a345ba5a75e3b1
Firose Hussain <github@firumon.com>
2019-07-10 10:52:33 +0530

    New interact for function table

---
+++ src/Interact/functionR.php
@@ -0,0 +1,55 @@
+<?php
+namespace Milestone\SS\Interact;
+
+use Milestone\SS\Model\Functiondetail;
+use Milestone\Interact\Table;
+use Milestone\SS\Model\WBin;
+
+class functionR implements Table
+{
+
+    public function getModel()
+    {
+        return WBin::class;
+    }
+
+    public function getImportAttributes()
+    {
+        return ['bin'];
+    }
+
+    public function getImportMappings()
+    {
+        return [
+            'bin' => 'processBin'
+        ];
+    }
+
+    public function processBin($record) {
+        $code = $record['CODE'];
+        $FN = Functiondetail::where(compact('code'))->first();
+        if($FN){
+            $FN->abr = $record['ABR'];
+            $FN->category = $record['CATEGORY'];
+            $FN->save();
+        }
+        return time();
+    }
+
+    public function getPrimaryIdFromImportRecord($data)
+    {
+        return null;
+    }
+
+
+    public function getExportMappings()
+    {
+        return [];
+    }
+
+    public function getExportAttributes()
+    {
+        return ['id','code','format','digit_length'];
+    }
+
+}
