8149e7ed6aa173b4d828612323d6edbf08177ff1
Firose Hussain <github@firumon.com>
2019-07-22 17:02:32 +0530

    Address field added to users table

--- migrations/2019_03_01_000001_add_ss_fields_in_users_table.php
+++ migrations/2019_03_01_000001_add_ss_fields_in_users_table.php
@@ -17,6 +17,7 @@ class AddSsFieldsInUsersTable extends Migration
             $table->string('email',191)->change();
             $table->char('reference',15)->nullable()->index()->after('email');
             $table->string('api_token',128)->nullable()->index()->after('email');
+            $table->string('address',512)->nullable()->after('email');
             $table->string('phone',128)->nullable()->index()->after('email');
         });
     }
@@ -29,7 +30,7 @@ class AddSsFieldsInUsersTable extends Migration
     public function down()
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->dropColumn(['reference','api_token','phone']);
+            $table->dropColumn(['reference','api_token','address','phone']);
         });
     }
 }
