b1551ee5c0aa3a2815af058ff3cfff6c445301af
Firose <firumon@github.com>
2019-08-13 20:15:46 +0530

    Resource change, seeders updated

--- src/Seeder/ResourceDataTableSeeder.php
+++ src/Seeder/ResourceDataTableSeeder.php
@@ -16,11 +16,11 @@ class ResourceDataTableSeeder extends Seeder
         $_ = \DB::statement('SELECT @@GLOBAL.foreign_key_checks');
         \DB::statement('set foreign_key_checks = 0');
         \Milestone\Appframe\Model\ResourceData::query()
-            ->create([	'id' => '327101', 	'resource' => '305118', 	'name' => 'TransactionProductNatureView', 	'description' => 'View details of a product transaction nature', 	'title_field' => 'name', 											])
-            ->create([	'id' => '327102', 	'resource' => '305119', 	'name' => 'TransactionProductTypeView', 	'description' => 'View details of a product transaction type', 	'title_field' => 'name', 											])
+            ->create([	'id' => '327101', 	'resource' => '305115', 	'name' => 'TransactionProductNatureView', 	'description' => 'View details of a product transaction nature', 	'title_field' => 'name', 											])
+            ->create([	'id' => '327102', 	'resource' => '305116', 	'name' => 'TransactionProductTypeView', 	'description' => 'View details of a product transaction type', 	'title_field' => 'name', 											])
             ->create([	'id' => '327103', 	'resource' => '305103', 	'name' => 'SettingsView', 	'description' => 'View details of a settings', 	'title_field' => 'name', 											])
-            ->create([	'id' => '327104', 	'resource' => '305115', 	'name' => 'UserSettingsView', 	'description' => 'View details of a user setting', 	'title_field' => 'user.name', 											])
-            ->create([	'id' => '327105', 	'resource' => '305116', 	'name' => 'UserStoreAreaView', 	'description' => 'View details of user store area', 	'title_field' => 'user.name', 											])
+            ->create([	'id' => '327104', 	'resource' => '305112', 	'name' => 'UserSettingsView', 	'description' => 'View details of a user setting', 	'title_field' => 'user.name', 											])
+            ->create([	'id' => '327105', 	'resource' => '305113', 	'name' => 'UserStoreAreaView', 	'description' => 'View details of user store area', 	'title_field' => 'user.name', 											])
         ;
         \DB::statement('set foreign_key_checks = ' . $_);
     }
--- src/Seeder/ResourceListTableSeeder.php
+++ src/Seeder/ResourceListTableSeeder.php
@@ -16,15 +16,15 @@ class ResourceListTableSeeder extends Seeder
         $_ = \DB::statement('SELECT @@GLOBAL.foreign_key_checks');
         \DB::statement('set foreign_key_checks = 0');
         \Milestone\Appframe\Model\ResourceList::query()
-            ->create([	'id' => '322101', 	'resource' => '305118', 	'name' => 'TransactionProductNature', 	'description' => 'Nature of the product in a transaction', 	'title' => 'Product Nature', 	'identity' => 'name', 	'items_per_page' => '20', 									])
-            ->create([	'id' => '322102', 	'resource' => '305119', 	'name' => 'TransactionProductType', 	'description' => 'Type of transaction whether is load, unload etc', 	'title' => 'Transaction Types', 	'identity' => 'name', 	'items_per_page' => '20', 									])
+            ->create([	'id' => '322101', 	'resource' => '305115', 	'name' => 'TransactionProductNature', 	'description' => 'Nature of the product in a transaction', 	'title' => 'Product Nature', 	'identity' => 'name', 	'items_per_page' => '20', 									])
+            ->create([	'id' => '322102', 	'resource' => '305116', 	'name' => 'TransactionProductType', 	'description' => 'Type of transaction whether is load, unload etc', 	'title' => 'Transaction Types', 	'identity' => 'name', 	'items_per_page' => '20', 									])
             ->create([	'id' => '322103', 	'resource' => '305103', 	'name' => 'Settings', 	'description' => 'List all available Settings', 	'title' => 'Settings', 	'identity' => 'name', 	'items_per_page' => '20', 									])
             ->create([	'id' => '322104', 	'resource' => '305101', 	'name' => 'ListAllUsers', 	'description' => 'List all available users', 	'title' => 'Users', 	'identity' => 'name', 	'items_per_page' => '50', 									])
-            ->create([	'id' => '322105', 	'resource' => '305115', 	'name' => 'ListAllUserSetting', 	'description' => 'List all user setting and value', 	'title' => 'User Settings', 	'identity' => 'user.name', 	'items_per_page' => '50', 									])
+            ->create([	'id' => '322105', 	'resource' => '305112', 	'name' => 'ListAllUserSetting', 	'description' => 'List all user setting and value', 	'title' => 'User Settings', 	'identity' => 'user.name', 	'items_per_page' => '50', 									])
             ->create([	'id' => '322106', 	'resource' => '305101', 	'name' => 'SalesExecutiveUserList', 	'description' => 'List all sales executives from users', 	'title' => 'Sales Executives', 	'identity' => 'name', 	'items_per_page' => '50', 									])
-            ->create([	'id' => '322107', 	'resource' => '305116', 	'name' => 'UserStoreAreaList', 	'description' => 'List all records in User Store Area', 	'title' => 'User, Store and Area', 	'identity' => 'user.name', 	'items_per_page' => '50', 									])
-            ->create([	'id' => '322108', 	'resource' => '305113', 	'name' => 'AreaList', 	'description' => 'List all areas', 	'title' => 'Area', 	'identity' => 'name', 	'items_per_page' => '50', 									])
-            ->create([	'id' => '322109', 	'resource' => '305112', 	'name' => 'StoresList', 	'description' => 'List all stores', 	'title' => 'Stores', 	'identity' => 'name', 	'items_per_page' => '50', 									])
+            ->create([	'id' => '322107', 	'resource' => '305113', 	'name' => 'UserStoreAreaList', 	'description' => 'List all records in User Store Area', 	'title' => 'User, Store and Area', 	'identity' => 'user.name', 	'items_per_page' => '50', 									])
+            ->create([	'id' => '322108', 	'resource' => '305110', 	'name' => 'AreaList', 	'description' => 'List all areas', 	'title' => 'Area', 	'identity' => 'name', 	'items_per_page' => '50', 									])
+            ->create([	'id' => '322109', 	'resource' => '305109', 	'name' => 'StoresList', 	'description' => 'List all stores', 	'title' => 'Stores', 	'identity' => 'name', 	'items_per_page' => '50', 									])
         ;
         \DB::statement('set foreign_key_checks = ' . $_);
     }
