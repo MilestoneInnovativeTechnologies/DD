01b74976191b7b42e3160994ae963c8310ccc09a
Firose <firumon@github.com>
2019-08-13 21:06:22 +0530

    Item master, removed narrations and group

--- src/Interact/itemmaster.php
+++ src/Interact/itemmaster.php
@@ -16,9 +16,7 @@ class itemmaster implements Table
     public function getImportAttributes()
     {
         return [
-            'code','name','narration','narration2','narration3','narration4','narration5','narration6','narration7',
-            'narration8','narration9','narration10','group1','group2','group3','group4','group5','group6','group7',
-            'group8','group9','group10','type','status'
+            'code','name','type','status'
         ];
     }
 
@@ -27,35 +25,11 @@ class itemmaster implements Table
         return array_merge([
             'code' => 'CODE',
             'name' => 'NAME',
-            'narration' => 'NARRATION',
-            'narration2' => 'NARRATION2',
-            'narration3' => 'NARRATION3',
-            'narration4' => 'NARRATION4',
-            'narration5' => 'NARRATION5',
-            'narration6' => 'NARRATION6',
-            'narration7' => 'NARRATION7',
-            'narration8' => 'NARRATION8',
-            'narration9' => 'NARRATION9',
-            'narration10' => 'NARRATION10',
             'type' => 'TYPE',
             'status' => 'STATUS'
         ],
             array_combine(array_map(function($n){ return 'group' . $n; },range(1,10)),
             array_map(function($n){ return 'getGroupValue' . $n; },range(1,10))));
-    }
-
-    public function __call($name, $arguments)
-    {
-        $num = str_replace('getGroupValue','',$name);
-        return $this->getGroupValue($num,$arguments[0]);
-    }
-
-    private function getGroupValue($group,$row){
-        $belongs = $row['CATCODE_'.str_pad($group,2,"0",STR_PAD_LEFT)];
-        $code = $row['GCODE_'.str_pad($group,2,"0",STR_PAD_LEFT)];
-        if(is_null($belongs) || is_null($code)) return null;
-        $product_group = Productgroup::where(['igmref' => implode('-',[$belongs,$code])])->first();
-        return $product_group ? $product_group->id : null;
     }
 
     public function getPrimaryIdFromImportRecord($data)
