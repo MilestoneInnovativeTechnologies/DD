c33fd49cb1625c51ae3c0a350d40af71d156090d
Firose <firumon@github.com>
2019-08-13 21:07:30 +0530

    Item master, removed narrations and group

--- src/Interact/itemmaster.php
+++ src/Interact/itemmaster.php
@@ -16,7 +16,7 @@ class itemmaster implements Table
     public function getImportAttributes()
     {
         return [
-            'code','name','type','status'
+            'code','name','narration','narration2','type','status'
         ];
     }
 
@@ -25,11 +25,27 @@ class itemmaster implements Table
         return array_merge([
             'code' => 'CODE',
             'name' => 'NAME',
+            'narration' => 'NARRATION',
+            'narration2' => 'NARRATION2',
             'type' => 'TYPE',
             'status' => 'STATUS'
         ],
             array_combine(array_map(function($n){ return 'group' . $n; },range(1,10)),
             array_map(function($n){ return 'getGroupValue' . $n; },range(1,10))));
+    }
+
+    public function __call($name, $arguments)
+    {
+        $num = str_replace('getGroupValue','',$name);
+        return $this->getGroupValue($num,$arguments[0]);
+    }
+
+    private function getGroupValue($group,$row){
+        $belongs = $row['CATCODE_'.str_pad($group,2,"0",STR_PAD_LEFT)];
+        $code = $row['GCODE_'.str_pad($group,2,"0",STR_PAD_LEFT)];
+        if(is_null($belongs) || is_null($code)) return null;
+        $product_group = Productgroup::where(['igmref' => implode('-',[$belongs,$code])])->first();
+        return $product_group ? $product_group->id : null;
     }
 
     public function getPrimaryIdFromImportRecord($data)
