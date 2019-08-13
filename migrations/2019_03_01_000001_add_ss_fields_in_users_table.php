04c4f59a7be097be03f4f978fb782a333dc74598
Firose <firumon@github.com>
2019-08-13 19:46:11 +0530

    Outstanding and Overdue fields added

--- migrations/2019_03_01_000001_add_ss_fields_in_users_table.php
+++ migrations/2019_03_01_000001_add_ss_fields_in_users_table.php
@@ -17,6 +17,8 @@ class AddSsFieldsInUsersTable extends Migration
             $table->string('email',191)->change();
             $table->char('reference',15)->nullable()->index()->after('email');
             $table->string('api_token',128)->nullable()->index()->after('email');
+            $table->decimal('overdue',30,10)->default(0)->after('email');
+            $table->decimal('outstanding',30,10)->default(0)->after('email');
             $table->string('address',512)->nullable()->after('email');
             $table->string('phone',128)->nullable()->index()->after('email');
         });
@@ -30,7 +32,7 @@ class AddSsFieldsInUsersTable extends Migration
     public function down()
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->dropColumn(['reference','api_token','address','phone']);
+            $table->dropColumn(['reference','api_token','address','phone','outstanding','overdue']);
         });
     }
 }
