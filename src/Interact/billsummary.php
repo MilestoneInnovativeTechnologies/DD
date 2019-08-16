91e56bc8a647f1b596d10c35de133273b83403b9
Firose <firumon@github.com>
2019-08-16 12:58:32 +0530

    Taxlist and Billsummary interacts added

---
+++ src/Interact/billsummary.php
@@ -0,0 +1,52 @@
+<?php
+
+
+    namespace Milestone\SS\Interact;
+
+    use Illuminate\Support\Arr;
+    use Milestone\SS\Model\User;
+
+    class billsummary implements Table
+    {
+        private $cache = [
+            'user' => null,
+        ];
+        public function getModel()
+        {
+            return User::class;
+        }
+
+        public function getImportAttributes()
+        {
+            return ['outstanding','overdue'];
+        }
+
+        public function getImportMappings()
+        {
+            return ['outstanding' => 'OUTSTANDING', 'overdue' => 'OVERDUE'];
+        }
+
+        public function getPrimaryIdFromImportRecord($data)
+        {
+            return $this->getUserId($data);
+        }
+
+        public function getExportAttributes()
+        {
+            return [];
+        }
+
+        public function getExportMappings()
+        {
+            return [];
+        }
+
+        public function getUserId($data){
+            if($this->cache['user'] === null) $this->cache['user'] = User::pluck('id','reference')->toArray();
+            return Arr::get($this->cache['user'],$data['ACCCODE'],null);
+        }
+
+        public function isValidImportRecord($record){
+            return ($record['ACCCODE'] && $this->getUserId($record));
+        }
+    }
---
+++ src/Interact/taxlist.php
@@ -0,0 +1,57 @@
+<?php
+
+
+    namespace Milestone\SS\Interact;
+
+    use Illuminate\Support\Arr;
+    use Milestone\SS\Model\Product;
+
+    class taxlist implements Table
+    {
+        private $cache = [
+            'product' => null,
+        ];
+        public function getModel()
+        {
+            return Product::class;
+        }
+
+        public function getImportAttributes()
+        {
+            return ['taxfactor','subtaxfactor','taxfactor02','subtaxfactor02'];
+        }
+
+        public function getImportMappings()
+        {
+            return [
+                'taxfactor' => 'TAXFACTOR',
+//                'subtaxfactor' => 'SUBTAXFACTOR',
+                'taxfactor02' => 'TAXFACTOR02',
+//                'subtaxfactor02' => 'SUBTAXFACTOR02'
+            ];
+        }
+
+        public function getPrimaryIdFromImportRecord($data)
+        {
+            return $this->getProductId($data);
+        }
+
+        public function getExportAttributes()
+        {
+            return [];
+        }
+
+        public function getExportMappings()
+        {
+            return [];
+        }
+
+        public function getProductId($data){
+            if($this->cache['product'] === null) $this->cache['product'] = Product::pluck('id','code')->toArray();
+            return Arr::get($this->cache['product'],$data['CODE'],null);
+        }
+
+        public function isValidImportRecord($record){
+            return ($record['CODE'] && $this->getProductId($record));
+        }
+    }
