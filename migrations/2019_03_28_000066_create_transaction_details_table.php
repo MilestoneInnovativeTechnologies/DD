5622c636858dde7271247ada4187567ef6a67e4f
Firose <firumon@github.com>
2019-10-04 12:12:34 +0530

    Pending migrations added

---
+++ migrations/2019_03_28_000066_create_transaction_details_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTransactionDetailsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('transaction_details', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->foreignCascade('transaction', 'transactions');
+            $table->foreignNullable('store', 'stores');
+            $table->foreignNullable('product', 'products');
+            $table->enum('direction', ['Out','In'])->default('Out');
+            $table->decimal('quantity', 30,10)->default(1);
+            $table->decimal('rate', 30,10)->default(0);
+            $table->char('taxrule', '15')->nullable();
+            $table->decimal('tax', 30,10)->default(0);
+            $table->decimal('discount01', 30,10)->default(0);
+            $table->decimal('discount02', 30,10)->default(0);
+            $table->foreignNullable('soi', 'sales_order_items');
+            $table->audit();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('transaction_details');
+    }
+}
