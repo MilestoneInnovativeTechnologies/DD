969dda6bc64d797de973ccf16fa3f3525827d831
Firose <firumon@github.com>
2019-10-16 10:36:47 +0530

    New scopes and appends added

--- src/Model/User.php
+++ src/Model/User.php
@@ -14,6 +14,13 @@ class User extends AppframeUser
             $builder->whereNotNull('users.reference');
         });
     }
+
+
+    protected $appends = ['name_login','name_reference'];
+    public function getNameLoginAttribute(){ return $this->name . ' (Login: ' . $this->login . ')'; }
+    public function getNameReferenceAttribute(){ return $this->name . ' (Reference: ' . $this->reference . ')'; }
+
+
     public function Area(){ return $this->belongsToMany(Area::class,'area_users','user','area'); }
     public function Settings(){ return $this->hasMany(UserSetting::class,'user')->with(['Settings']); }
     public function StoreAndArea(){ return $this->hasMany(UserStoreArea::class,'user')->with(['Store','Area']); }
@@ -23,8 +30,8 @@ class User extends AppframeUser
     public function Groups(){ return $this->belongsToMany(Group::class,'__group_users','user','group'); }
 
     public function scopeSalesExecutive($q){
-        return $q->whereHas('Groups',function($q){
-            $q->where('reference','SLS');
-        });
+        return $q->whereHas('Groups',function($q){ $q->where('reference','SLS'); });
     }
+    public function scopeLoginUsers($q){ return $q->whereNotNull('login'); }
+    public function scopeLoginExecutives($q){ return $q->salesExecutive()->loginUsers()->where('reference','LIKE','SE%'); }
 }
